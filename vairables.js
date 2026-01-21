41b87c0053f68dcf08f140d3c673920ee2d2bb17
anisalubaiana <auroraivy456@gmail.com>
2026-01-21 16:57:36 +0600

    add variables.js

---
+++ vairables.js
@@ -0,0 +1,10 @@
+console.log("This is vairables.js file");
+let name = "KHAN LUBAIANA ANISA";
+let age = 22;
+let email = "auroraivy456@gmail.com";
+let interest = "Cooking,journaling,singing;";
+console.log("Name: " ,name);
+console.log("Age: " , age);
+console.log("Email: " ,email);
+console.log("Interest: " , interest);
+
